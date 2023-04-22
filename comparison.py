643509ed552a2b459442ce74b8497a5f2d66da66
Kyl <125286137+kylwrld@users.noreply.github.com>
2023-04-22 04:28:58 -0300

    fix bugs that i don't remember

--- comparison.py
+++ comparison.py
@@ -3,13 +3,16 @@ def comparison(list1, list2):
     following = []
     notFollowing = []
     
-    if list1 == list2:
+    list1l = list1[0].split(" ")
+    list2l = list2[0].split(" ")
+    
+    if list1l == list2l:
         print("Nothing changed.")
     else:
         # Checks if an account in the new list ISN'T in the old list, 
         # this account is whom the user you choose to scan data followed.
-        for i in list1:
-            if i not in list2:
+        for i in list1l:
+            if i not in list2l:
                 following.append(i)
         
         followingStr = " ".join(following)        
@@ -18,8 +21,8 @@ def comparison(list1, list2):
 
         # Checks if an account in the old list ISN'T in the new list, 
         # this account is whom the user you chose to scan data has stopped following.
-        for i in list2:
-            if i not in list1:
+        for i in list2l:
+            if i not in list1l:
                 notFollowing.append(i)    
 
         notFollowingStr = " ".join(notFollowing)
